6ff54ff1671e7300810eb3309f299ca5c6532776
Chanman02222002 <141520647+Chanman02222002@users.noreply.github.com>
2025-11-21 02:39:52 -0500

    Update app.py

--- newdociv/app.py
+++ newdociv/app.py
@@ -39,6 +39,7 @@ import json
 import os
 import re
 import pandas as pd
+import time
 
 from io import BytesIO
 import openai
@@ -2485,6 +2486,7 @@ threading.Thread(target=open_browser).start()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
